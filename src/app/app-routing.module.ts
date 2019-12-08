90095a491701ee4ed9f2849a6e332d12efd71cee
AugustoBondanca <augustobondancaa@gmail.com>
2019-12-08 01:57:16 -0300

    pagina de tutorial criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'tutorial', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'tutorial', loadChildren: () => import('./tutorial/tutorial.module').then( m => m.TutorialComponentModule)},
 ];
