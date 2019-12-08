b45e6fc07eaddbbdad3bd19f765121f8c6fcab6d
AugustoBondanca <augustobondancaa@gmail.com>
2019-12-08 01:28:58 -0300

    tela de tutorial criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'tutorial', loadChildren: () => import('./tutorial/tutorial.module').then( m => m.TutorialComponentModule)},
 ];
 
 @NgModule({
